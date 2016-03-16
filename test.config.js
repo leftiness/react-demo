bfe1b0e2a13736fd19d167ea2d1cff56a3cbab4d
Brandon Parmenter <leftiness@gmail.com>
2016-03-15 22:21:18 -0400

    Webpack handles testing now :)
    npm start. Then npm test in another console. One is on port 5000. The
    other is on 5001. Both will update whenever changes happen. Refresh the
    test page to see the test results. Refresh the other to use the app.

---
+++ test.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var babel_loader = {
+  test: /.jsx?$/,
+  loader: 'babel-loader',
+  exclude: /node_modules/,
+  query: {
+    presets: [ 'es2015', 'react', 'stage-1' ]
+  }
+};
+
+module.exports = {
+  entry: "mocha!./test/index.js",
+  module: {
+    loaders: [ babel_loader ]
+  }
+};
