5e29acde9cbad6eaed9a6ba496e7c0d54138e4df
Brandon Parmenter <leftiness@gmail.com>
2016-04-23 18:14:06 -0400

    Removed link from notfound page

--- components/NotFound.jsx
+++ components/NotFound.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-import NavigationLink from 'containers/NavigationLink.jsx';
-
 const NotFound = () => (
-  <div>
-    <h2>404 :(</h2>
-    <NavigationLink name="dates">Dates</NavigationLink>
-  </div>
+  <h2>404 :(</h2>
 );
 
 export default NotFound;
