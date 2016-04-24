efc835b89ea32ddee6d107a5906df11eb1da51eb
Brandon Parmenter <leftiness@gmail.com>
2016-04-23 21:50:10 -0400

    Latest version of picnic isn't on npm

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 
-const Picnic = require('picnic/releases/picnic.min.css');
-const PicnicPlugins = require('picnic/releases/plugins.min.css');
+const Picnic = require('picnic/picnic.min.css');
 
 import * as reducers from './reducers';
 import ApplicationContainer from 'containers/ApplicationContainer.jsx';
