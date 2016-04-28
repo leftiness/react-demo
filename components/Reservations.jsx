7cd3d7a5169d9bafb4b8ad11de23ad7539b4bb35
Brandon Parmenter <leftiness@gmail.com>
2016-04-27 23:22:53 -0400

    Added super basic reservation stuff
    - Reservation component
    - Reservations component with mapped reservations
    - Some wip unit test stuff

--- components/Reservations.jsx
+++ components/Reservations.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { map } from 'lodash';
 
-const Reservations = () => (
-  <h2>Hello</h2>
+import Reservation from './Reservation.jsx';
+
+const Reservations = ({ reservations = [] }) => (
+  <div>
+    <h2>Hello</h2>
+    {
+      reservations.length === 0
+        ? <div>There are no reservations.</div>
+        : map(reservations, (res) => {
+          return <Reservation />
+        })
+    }
+  </div>
 );
+
+Reservations.propTypes = {
+  reservations: PropTypes.array
+};
 
 export default Reservations;
--- test/components/Reservations.spec.js
+++ test/components/Reservations.spec.js
@@ -1,20 +1,66 @@
 import utils from 'react-addons-test-utils';
 import React from 'react';
 import expect from 'expect.js';
+import { get, curry } from 'lodash';
 
 import Reservations from 'components/Reservations.jsx';
 
 describe('Reservations', () => {
   const renderer = utils.createRenderer();
-  const element = React.createElement(Reservations);
-  it('should be an h2', () => {
-    renderer.render(element);
-    const result = renderer.getRenderOutput();
-    expect(result.type).to.be('h2');
+  const result = curry((element, props) => {
+    return React.createElement(element, props);
+  })(Reservations);
+  const empty = result([]);
+  const one = result([ 'foo' ]);
+
+  it('should be a reservations component', () => {
+    const atype = get(empty, 'type.name');
+    expect(atype).to.be('Reservations');
   });
+
+  /*
+  TODO
+  Figure out this unit testing stuff...
+
+  it('should have an h2', () => {
+    const atype = get(empty, 'props.children[0].type');
+    expect(atype).to.eql('h2');
+  });
+
   it('should say hello', () => {
-    renderer.render(element);
-    const result = renderer.getRenderOutput();
-    expect(result.props.children).to.eql('Hello');
+    const children = get(empty, 'props.children[0].children');
+    expect(children).to.eql('Hello');
   });
+
+  it('should have a div', () => {
+    const atype = get(empty, 'props.children[1].type');
+    expect(atype).to.eql('div');
+  });
+
+  it('should say that there are no reservations', () => {
+    const children = get(empty, 'props.children[1].children');
+    expect(children).to.eql('There are no reservations.');
+  });
+
+  it('should have an array', () => {
+    const children = get(one, 'props.children[1].children');
+    expect(children).to.be.an('array');
+  });
+
+  it('should have one item', () => {
+    const length = get(one, 'props.children[1].children.length');
+    expect(length).to.eql(1);
+  });
+
+  it('should be a reservation', () => {
+    const name = get(one, 'props.children[1].children.type.name');
+    expect(name).to.eql('Reservation');
+  });
+
+  it('should have the right props', () => {
+    // ...
+  });
+
+  */
+
 });
