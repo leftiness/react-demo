f7185a6e10e052ae69d33d1db4ac1162667c145a
Brandon Parmenter <leftiness@gmail.com>
2016-05-01 00:53:25 -0400

    wip ADD_RESERVATION functionality
    Input form related to issue #47

--- components/Reservations.jsx
+++ components/Reservations.jsx
@@ -3,22 +3,36 @@ import { isEmpty, map, keys } from 'lodash';
 
 import Reservation from './Reservation.jsx';
 
-const Reservations = ({ reservations = {} }) => (
-  <div>
-    <h2>Hello</h2>
-    {
-      isEmpty(reservations)
-        ? <div>There are no reservations.</div>
-        : map(keys(reservations), (key) => {
-          const reservation = reservations[key];
-          return <Reservation { ...reservation } />
-        })
-    }
-  </div>
-);
+const Reservations = ({ reservations = {}, onAddReservation }) => {
+  let nameInput;
+  const add = (input) => {
+    onAddReservation(input.value);
+    input.value = '';
+  };
+  return (
+    <div>
+      <h2>Hello</h2>
+      {
+        isEmpty(reservations)
+          ? <div>There are no reservations.</div>
+          : map(keys(reservations), (key) => {
+            const reservation = reservations[key];
+            return <Reservation key={ reservation.id }{ ...reservation } />
+          })
+      }
+      <input
+        ref={ (input) => nameInput = input }
+        placeholder="Name"
+        onKeyDown={ (e) => e.keyCode === 13 ? add(nameInput) : false }
+      />
+      <button onClick={ () => add(nameInput) }>Add</button>
+    </div>
+  );
+}
 
 Reservations.propTypes = {
-  reservations: PropTypes.object
+  reservations: PropTypes.object,
+  onAddReservation: PropTypes.func.isRequired,
 };
 
 export default Reservations;
--- containers/ReservationsContainer.jsx
+++ containers/ReservationsContainer.jsx
@@ -1,12 +1,26 @@
 import { connect } from 'react-redux';
 
 import Reservations from 'components/Reservations.jsx';
+import { addReservation } from 'actions/reservations.js';
+
+/*
+ * Eventually, I'll ask a service to create a reservation record.
+ * If the service succeeds, the promise will have an id. Then I'll send that
+ * to redux. For now, I'm just going to increment this value.
+ */
+let tempid = 0;
 
 const mapStateToProps = (state) => {
   const reservations = state.reservations;
   return { reservations };
 };
 
+const mapDispatchToProps = (dispatch) => {
+  const onAddReservation = (name) => dispatch(addReservation(tempid++, name));
+  return { onAddReservation };
+}
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps,
 )(Reservations);
