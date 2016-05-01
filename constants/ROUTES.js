9bc97f0a330347b2e13144a5a9fb3cdd5e3ca319
Brandon Parmenter <leftiness@gmail.com>
2016-04-30 23:42:37 -0400

    Reservations route points to new container

--- constants/ROUTES.js
+++ constants/ROUTES.js
@@ -1,5 +1,5 @@
 import Home from 'components/Home.jsx';
-import Reservations from 'components/Reservations.jsx';
+import ReservationsContainer from 'containers/ReservationsContainer.jsx';
 import NotFound from 'components/NotFound.jsx';
 
 const route = (text, path, component) => {
@@ -8,7 +8,11 @@ const route = (text, path, component) => {
 
 const ROUTES = {
   home: route('Home', 'GET /home', Home),
-  reservations: route('Reservations', 'GET /reservations', Reservations)
+  reservations: route(
+    'Reservations',
+    'GET /reservations',
+    ReservationsContainer,
+  ),
 };
 
 export default ROUTES;
