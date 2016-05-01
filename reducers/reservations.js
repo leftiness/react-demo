fb3c3b515f30784fcee1c1ec356c6035c8143cfa
Brandon Parmenter <leftiness@gmail.com>
2016-04-30 23:22:05 -0400

    Added add / delete to reservation reducer

--- reducers/reservations.js
+++ reducers/reservations.js
@@ -1,8 +1,16 @@
-const reservations = (state = {}, action) => {
+import { assign, omit, flow } from 'lodash/fp';
+
+export default (state = {}, action = {}) => {
   switch (action.type) {
+    case 'ADD_RESERVATION':
+      return flow(
+        omit('type'),
+        (a) => ({ [a.id] : { ...a }}),
+        assign(state),
+      )(action);
+    case 'DELETE_RESERVATION':
+      return omit(action.id)(state);
     default:
       return state;
   }
 };
-
-export default reservations;
--- test/reducers/reservations.spec.js
+++ test/reducers/reservations.spec.js
@@ -1,11 +1,48 @@
 import expect from 'expect.js';
+import { get, set } from 'lodash';
 
 import reservations from 'reducers/reservations.js';
 
 export default describe('reservations reducer', () => {
+
   it('should handle initial state', () => {
-    expect(
-      reservations(undefined, {})
-    ).to.eql({})
-  })
+    const result = reservations();
+    expect(result).to.be.empty();
+  });
+
+  it('should create a new reservation', () => {
+    const atype = 'ADD_RESERVATION';
+    const id = '0';
+    const name = 'Melody';
+    const action = { type: atype, id, name };
+    const result = reservations({}, action);
+    const record = get(result, id);
+    expect(result).to.only.have.key(id);
+    expect(record).to.have.property('id', id);
+    expect(record).to.have.property('name', name);
+  });
+
+  it('should update an existing reservation', () => {
+    const atype = 'ADD_RESERVATION';
+    const id = '0';
+    const name = 'Melody';
+    const changed = 'Felicity';
+    const state = set({}, id, { id, name });
+    const action = { type: atype, id, name: changed };
+    const result = reservations(state, action);
+    const record = get(result, id);
+    expect(result).to.only.have.key(id);
+    expect(record).to.have.property('id', id);
+    expect(record).to.have.property('name', changed);
+  });
+
+  it('should delete a reservation', () => {
+    const atype = 'DELETE_RESERVATION';
+    const id = '0';
+    const state = set({}, id, { id });
+    const action = { type: atype, id };
+    const result = reservations(state, action);
+    expect(result).to.be.empty();
+  });
+
 });
